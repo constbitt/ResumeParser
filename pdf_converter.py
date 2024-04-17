8d29a8459c7ff0b645d7e0c0e33043dffd19d26d
constbitt <ms.biderina@mail.ru>
2024-04-17 13:48:33 +0300

    use spire.doc

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,20 +1,17 @@
 import os
 import tempfile
-
-import comtypes.client
 import docx
 import pdfkit
 import markdown2
 import shutil
 from docx import Document
-
+#from docx2pdf import convert
+from spire.doc import *
+from spire.doc.common import *
 
 class FileConverter:
     def __init__(self):
-        comtypes.CoInitialize()
-
-    def __del__(self):
-        comtypes.CoUninitialize()
+        self.data = []
 
     def convert_to_pdf(self, input_file, output_file):
         _, file_extension = os.path.splitext(input_file)
@@ -23,16 +20,16 @@ class FileConverter:
             shutil.copyfile(input_file, output_file)
         if file_extension == '.txt':
             self.txt_pdf_converter(input_file, output_file)
-        elif file_extension == '.rtf':
-            self.rtf_pdf_converter(input_file, output_file)
-        elif file_extension == '.docx':
+        elif file_extension == '.docx' or file_extension == '.doc' or file_extension == '.rtf':
             self.docx_pdf_converter(input_file, output_file)
+        #elif file_extension == '.doc':
+        #    self.doc_pdf_converter(input_file, output_file)
+        #elif file_extension == '.rtf':
+        #    self.rtf_pdf_converter(input_file, output_file)
         elif file_extension == '.html' or file_extension == '.htm':
             self.html_pdf_converter(input_file, output_file)
         elif file_extension == '.md':
             self.md_pdf_converter(input_file, output_file)
-        elif file_extension == '.doc':
-            self.doc_pdf_converter(input_file, output_file)
         else:
             print("Unsupported file format")
 
@@ -43,6 +40,13 @@ class FileConverter:
         self.docx_pdf_converter(tmp_file, output_file)
         os.remove(tmp_file)
 
+    def docx_pdf_converter(self, input_file, output_file):
+        input_file = os.path.abspath(input_file)
+        output_file = os.path.abspath(output_file)
+        document = Document()
+        document.LoadFromFile(input_file)
+        document.SaveToFile(output_file, FileFormat.PDF)
+        document.Close()
     def txt_docx_converter(self, input_file, output_file):
         with open(input_file, 'r', encoding='utf-8') as file:
             lines = file.readlines()
@@ -51,38 +55,14 @@ class FileConverter:
             doc.add_paragraph(line.strip())
         doc.save(output_file)
 
-    def rtf_pdf_converter(self, input_file, output_file):
-        wdFormatPDF = 17
-        input_file = os.path.abspath(input_file)
-        output_file = os.path.abspath(output_file)
-        word = comtypes.client.CreateObject("Word.Application")
-        word.Visible = False
-        doc = word.Documents.Open(input_file)
-        doc.SaveAs(output_file, FileFormat=wdFormatPDF)
-        doc.Close()
-        word.Quit()
+    #def docx_pdf_converter(self, input_file, output_file):
+    #    input_file = os.path.abspath(input_file)
+    #    output_file = os.path.abspath(output_file)
+    #    convert(input_file, output_file)
 
-    def docx_pdf_converter(self, input_file, output_file):
-        pdf_format = 17
-        input_file = os.path.abspath(input_file)
-        output_file = os.path.abspath(output_file)
-        word = comtypes.client.CreateObject("Word.Application")
-        word.Visible = False
-        doc = docx.Document(input_file)
-        in_file = word.Documents.Open(input_file)
-        in_file.SaveAs(output_file, FileFormat=pdf_format)
-        in_file.Close()
-        word.Quit()
+    #def doc_pdf_converter(self, input_file, output_file):
 
-    def doc_pdf_converter(self, input_file, output_file):
-        wdFormatPDF = 17
-        word = comtypes.client.CreateObject("Word.Application")
-        input_file = os.path.abspath(input_file)
-        output_file = os.path.abspath(output_file)
-        doc = word.Documents.Open(input_file)
-        doc.SaveAs(output_file, FileFormat=wdFormatPDF)
-        doc.Close()
-        word.Quit()
+    #def rtf_pdf_converter(self, input_file, output_file):
 
     def html_pdf_converter(self, input_file, output_file):
         pdfkit.from_file(input_file, output_file)
@@ -92,4 +72,3 @@ class FileConverter:
             markdown_content = file.read()
         html_content = markdown2.markdown(markdown_content)
         pdfkit.from_string(html_content, output_file)
-
