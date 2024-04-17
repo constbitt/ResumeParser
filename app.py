e19ca5d5e14d1511285760e0df5ad87be1043620
constbitt <ms.biderina@mail.ru>
2024-04-17 14:26:56 +0300

    remove tmpdir

--- app.py
+++ app.py
@@ -39,36 +39,32 @@ def index():
             file = request.files.get('file_input')
             language = request.form.get('language')
             if file:
-                # Создаем временный файл для сохранения загруженного файла
-                temp_dir = tempfile.gettempdir()
-                temp_path = os.path.join(temp_dir, file.filename)
+                # Сохраняем файл в папку static
+                static_dir = os.path.join(app.root_path, 'static')
+                temp_path = os.path.join(static_dir, file.filename)
                 file.save(temp_path)
                 file_name = file.filename
+
                 # Код обработки загруженного файла
                 try:
-                    cv_holder_name, birth, numbers, mails, links, education, languages = extract_attributes(temp_path, language)
+                    cv_holder_name, birth, numbers, mails, links, education, languages = extract_attributes(temp_path,
+                                                                                                            language)
                 except:
                     return render_template('index.html', error_message='Wrong file format!')
 
                 # Конвертация в pdf для предпросмотра
-                static_dir = os.path.join(app.root_path, 'static')
-                for filename in os.listdir(static_dir):
-                    file_path = os.path.join(static_dir, filename)
-                    if os.path.isfile(file_path):
-                        os.remove(file_path)
-
                 file_name, file_extension = os.path.splitext(file_name)
                 pdf_name = file_name + '.pdf'
                 converter = FileConverter()
                 pdf_path = os.path.join(static_dir, pdf_name)
                 converter.convert_to_pdf(temp_path, pdf_path)
 
-                # Удаление временного файла
-                os.remove(temp_path)
     except:
         return render_template('index.html', error_message='Something went wrong!')
+
     return render_template('index.html', text=text, file_name=pdf_name, cv_holder_name=cv_holder_name, birth=birth,
-                           numbers=numbers, mails=mails, education=education, links=links, languages=languages, error_message=None)
+                           numbers=numbers, mails=mails, education=education, links=links, languages=languages,
+                           error_message=None)
 
 
 @app.route('/copy_data', methods=['POST'])
